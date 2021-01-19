df665bb13814f3ecce2f09aeb4d3a0a47ab469fa
jessijoke <jessijoke@yahoo.com>
2021-01-18 22:35:28 -0500

    commit tree 2479c3bb02f06cbd8f621564040705eee8419825

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do
+        "Hello, World!"
+    end
+
+    get '/name' do
+        "My name is Jessica"
+    end
+
+    get '/hometown' do
+        "My hometown is Holland"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Getaway Car"
+    end
 end
